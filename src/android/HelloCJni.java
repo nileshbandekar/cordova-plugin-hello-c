66e80b9853bf98862f2eb68179b4506d444c7ca1
nileshbandekar <44316189+nileshbandekar@users.noreply.github.com>
2021-04-09 20:34:46 +0530

    Update HelloCJni.java

--- src/android/HelloCJni.java
+++ src/android/HelloCJni.java
@@ -8,8 +8,15 @@ public class HelloCJni {
     public static native int calculate(int x, int y);
     public static native int crash();
 
-    // load library
+    // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("helloc");
+        System.loadLibrary("native-lib");
     }
+    
+    
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native void init();
 }
